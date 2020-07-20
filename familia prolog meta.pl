72a12c7fe6176ab807eb307f0925d3e67126d23a
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-07-20 17:11:27 -0500

    Update familia prolog meta.pl

--- familia prolog meta.pl
+++ familia prolog meta.pl
@@ -25,7 +25,14 @@ nieto(A,B):- hijo(B,C),hijo(C,A).
 tio(A,B):- hermano(A,C), es_padre(C,B).
 sobrino(A,B):- hermano(B,C), hijo(A,C).
 primo(A,B):- es_padre(C,B), es_padre(D,A), hermano(C,D).
-familiar(A,B):- es_padre(A,B);
+familiar(A,B):-
+  padre(A,B);
+  hijo(A,B);
+  abuelo(A,B);
+  nieto(A,B);
+  tio(A,B);
+  sobrino(A,B);
+  primo(A,B).
 familiares(A,L):-findall(B,familiar(A,B),L).
 casado(A,B):- es_padre(A,C),es_padre(B,C).
 feliz(A) :- es_casado(A,B).
